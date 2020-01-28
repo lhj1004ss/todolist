e193e2c1b0295a2380e77317396977da4c1cee48
hyojelee <hyojelee1004ss@gmail.com>
2020-01-28 17:39:27 -0500

    todolist

---
+++ src/components/TodoList.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+export default class TodoList extends Component {
+  render() {
+    return <div>hello from todolist</div>;
+  }
+}
