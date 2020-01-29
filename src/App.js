cc3a68b82d6309e27076461b598dfc576f19fa94
hyojelee <hyojelee1004ss@gmail.com>
2020-01-29 17:32:37 -0500

    add clear,delete function

--- src/App.js
+++ src/App.js
@@ -4,13 +4,65 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 export default class App extends Component {
+  state = {
+    items: [],
+    id: uuid(), //create random id whenever page is refreshed
+    item: "",
+    editItem: false
+  };
+  handleChange = e => {
+    this.setState({
+      item: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id, //grab random uuid
+      title: this.state.item // grab item whatever user type (now empty)
+    };
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuid(),
+      editItem: false
+    });
+  };
+  clearList = () => {
+    this.setState({
+      items: []
+    });
+  };
+  handleDelete = id => {
+    const filterItems = this.state.items.filter(item => item.id !== id);
+    this.setState({
+      items: filterItems
+    });
+  };
+  handleEdit = id => {
+    console.log(`handle edit ${id}`);
+  };
   render() {
+    console.log(this.state);
     return (
-      <div>
-        <div className="container">
-          <div className="row">
-            <TodoInput></TodoInput>
-            <TodoList></TodoList>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            ></TodoInput>
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            ></TodoList>
           </div>
         </div>
       </div>
